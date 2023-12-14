e08a004844d5ff826a7e9840afd536ae789a9094
Tego <tego101@users.noreply.github.com>
2023-12-14 17:42:40 -0500

    Update m3usort.py

--- m3usort.py
+++ m3usort.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """
 #######################################################
 #   TM3U Sort                                         #
@@ -13,124 +14,123 @@
 #   ALL DONATIONS WILL BE USED FOR SUSHI & .. SUSHI   #
 #######################################################
 """
+
 import os
 import glob
 import time
 
+
 class TM3U:
     @classmethod
-    def intro(self):
+    def intro(cls):
         #   Intro.
-        print('\n########################################################')
-        print('#   TM3U Sort                                          #')
-        print('#   A Quick tool to sort out elements in a m3u file.   #')
-        print('#   ###########################                        #')
-        print('#   http://github.com/tego101                          #')
-        print('#   ####################################               #')
-        print('#   # IF THIS HELPED, PLEASE DONATE :) #               #')
-        print('#   ####################################               #')
-        print('#   BITCOIN -> 1KouUcQ7o4rVh8gTg6W3HPUDsBBdoC6G1K      #')
-        print('#   CASHAPP -> $redtvme                                #')
-        print('########################################################')
-        print('#    ALL DONATIONS WILL BE USED FOR SUSHI & .. SUSHI   #')
-        print('########################################################\n')
-        print('### THANK YOU TO ALL MY SUPPORTERS!!  ##################')
-        print('########################################################')
-        print('#### @realiptv                                      ####')
-        print('#### @TheOGKillz                                    ####')
-        print('#### @@Ultimate_scraper                             ####')
-        print('#### @codelinux89                                   ####')
-        print('#### @billybadson                                   ####')
-        print('#### @cafterburn77                                  ####')
-        print('#### @ytvm4u                                        ####')
-        print('#### @jbrad                                         ####')
-        print('########################################################')
-        print('#### ==> <3 <3 I APPERCIATE YOU GUYS!!!! <3 <3 <=== ####')
-        print('########################################################\n')
+        intro_text = "\n########################################################\n"
+        intro_text += "#   TM3U Sort                                          #\n"
+        intro_text += "#   A Quick tool to sort out elements in a m3u file.   #\n"
+        intro_text += "#   ###########################                        #\n"
+        intro_text += "#   http://github.com/tego101                          #\n"
+        intro_text += "#   ####################################               #\n"
+        intro_text += "#   # IF THIS HELPED, PLEASE DONATE :) #               #\n"
+        intro_text += "#   ####################################               #\n"
+        intro_text += "#   BITCOIN -> 1KouUcQ7o4rVh8gTg6W3HPUDsBBdoC6G1K      #\n"
+        intro_text += "#   CASHAPP -> $redtvme                                #\n"
+        intro_text += "########################################################\n"
+        intro_text += "#    ALL DONATIONS WILL BE USED FOR SUSHI & .. SUSHI   #\n"
+        intro_text += "########################################################\n\n"
+        intro_text += "### THANK YOU TO ALL MY SUPPORTERS!!  ##################\n"
+        intro_text += "########################################################\n"
+        intro_text += "#### @realiptv                                      ####\n"
+        intro_text += "#### @TheOGKillz                                    ####\n"
+        intro_text += "#### @@Ultimate_scraper                             ####\n"
+        intro_text += "#### @codelinux89                                   ####\n"
+        intro_text += "#### @billybadson                                   ####\n"
+        intro_text += "#### @cafterburn77                                  ####\n"
+        intro_text += "#### @ytvm4u                                        ####\n"
+        intro_text += "#### @jbrad                                         ####\n"
+        intro_text += "########################################################\n"
+        intro_text += "#### ==> <3 <3 I APPERCIATE YOU GUYS!!!! <3 <3 <=== ####\n"
+        intro_text += "########################################################\n\n"
+
+        print(intro_text)
+
         time.sleep(1)
 
     @classmethod
-    def outro(self):
+    def outro(cls):
         #   Pay homage to my first contributors for this script.
         #   If this helped you please donate!
-        print('==> THANK YOU TO MY SUPPORTERS!!! \n ')
-        time.sleep(1)
-        print(' ==> @realIPTV (BOSS OF STREAMS!) \n')
-        time.sleep(1)
-        print('   ==> @TheOGKillz \n')
-        time.sleep(1)
-        print('    ==> @Ultimate_scraper \n')
-        time.sleep(1)
-        print('     ==> @codelinux89 \n')
-        time.sleep(1)
-        print('      ==> @billybadson \n')
-        time.sleep(1)
-        print('       ==> @cafterburn77 \n')
-        time.sleep(1)
-        print('        ==> @ytvm4u \n')
-        time.sleep(1)
-        print('         ==> @jbrad \n')
-        time.sleep(1)
-        print('          ==> <3 <3 I APPERCIATE YOU GUYS!!!! \n')
+        outro_text = "==> THANK YOU TO MY SUPPORTERS!!! \n\n"
+        outro_text += " ==> @realIPTV (BOSS OF STREAMS!) \n\n"
+        outro_text += "   ==> @TheOGKillz \n\n"
+        outro_text += "    ==> @Ultimate_scraper \n\n"
+        outro_text += "     ==> @codelinux89 \n\n"
+        outro_text += "      ==> @billybadson \n\n"
+        outro_text += "       ==> @cafterburn77 \n\n"
+        outro_text += "        ==> @ytvm4u \n\n"
+        outro_text += "         ==> @jbrad \n\n"
+        outro_text += "          ==> <3 <3 I APPERCIATE YOU GUYS!!!! \n\n"
+
+        print(outro_text)
+
         time.sleep(1)
 
     @classmethod
-    def localList(self):
-        print('\n')
+    def local_list(cls):
+        print("\n")
 
         files = []
-        print(' ###### Available Lists to Scan: ######\n')
-        print(' Please place your files in the "lists" folder to scan them. \n')
+        print(" ###### Available Lists to Scan: ######\n")
+        print(" Please place your files in the 'lists' folder to scan them. \n")
         for file in glob.glob("lists/*.m3u"):
             alists = file
             print(alists.replace("lists/", "----> "))
 
-        print('\n')
+        print("\n")
 
         #   Request File Name.
-        r_file = 'lists/' + input(' Enter File Name: ')
+        r_file = 'lists/' + input(" Enter File Name: ")
 
         #   Request year or element to search.
         if not os.path.isfile(r_file):
-            print('\n')
-            print(' ### ERROR ### ---> ' + r_file.replace("lists/", " ") + '.m3u Does not exist! <---- ### ERROR ### \n')
+            print("\n")
+            print(" ### ERROR ### ---> " + r_file.replace("lists/", " ") + '.m3u Does not exist! <---- ### ERROR ### \n')
 
             #   Search again
-            again = input(' Try Again? (Y/N)')
+            again = input(" Try Again? (Y/N)")
             #   Do the dooo
-            if again is 'Y' or again is 'y':
-                TM3U.localList()
+            if again.lower() == 'y':
+                cls.local_list()
             else:
-                print('\n OK! \n')
-                print('########################')
-                TM3U.outro()
+                print("\n OK! \n")
+                print("########################")
+                cls.outro()
         else:
-            print('\n')
-            r_year_title = input(' What Year or Title? ')
+            print("\n")
+            r_year_title = input(" What Year or Title? ")
 
             #   Lets open up the requested file and search it.
             with open(r_file) as input_file:
                 content_generator = (line for line in input_file.read().splitlines())
-                print('\n')
+                print("\n")
                 #   Iter the results.
-                print(' #### RESULTS ####\n')
+                print(" #### RESULTS ####\n")
                 for line in content_generator:
                     if r_year_title in line:
                         next_line = next(content_generator)
                         print(line)
                         print(next_line)
 
-                print('\n')
+                print("\n")
 
                 #   Save List to File.
-                save = input(' Save to file? Yes or No: \n ')
-                print('\n')
+                save = input(" Save to file? Yes or No: \n ")
+                print("\n")
                 #   Save the file?
-                if save == 'YES' or save == 'Yes' or save == 'yes':
+                if save.lower() == 'yes':
                     #   Name the file.
-                    file_name = input(' What should we name the file? \n   ')
+                    file_name = input(" What should we name the file? \n   ")
 
-                    new_list = open('saved/' + file_name + '.m3u','w+')
+                    new_list = open('saved/' + file_name + '.m3u', 'w+')
                     new_list.write("#EXTM3U \n")
 
                     with open(r_file) as input_file:
@@ -142,24 +142,23 @@ class TM3U:
                                 new_list.write(line + '\n')
                                 new_list.write(next_line + '\n')
 
-
-
                         #   Process complete!!!
-                        print(' \n Operation Complete!!! file saved as ' + file_name + '.m3u in the SAVED folder! \n')
+                        print(" \n Operation Complete!!! file saved as " + file_name + '.m3u in the SAVED folder! \n')
 
                         #   Search again?
-                        again = input(' Search Again? (Y/N) \n ')
+                        again = input(" Search Again? (Y/N) \n ")
 
-                        if again is 'Y' or again is 'y':
-                            TM3U.localList()
+                        if again.lower() == 'y':
+                            cls.local_list()
                         else:
-                            print('\n OK! \n')
-                            print('########################')
-                            TM3U.outro()
+                            print("\n OK! \n")
+                            print("########################")
+                            cls.outro()
 
-                elif save == 'NO' or save == 'No' or save == 'no':
-                        print('OK!')
+                elif save.lower() == 'no':
+                    print("OK!")
 
 
-TM3U.intro()
-TM3U.localList()
+if __name__ == "__main__":
+    TM3U.intro()
+    TM3U.local_list()
